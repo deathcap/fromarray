33ecb285d1aca1482c2e17ad7c3d86aa647372cd
deathcap <deathcap@gmx.co.uk>
2014-04-27 11:53:09 -0700

    Add array-like object tests (see also: asarray)

--- test.js
+++ test.js
@@ -19,16 +19,23 @@ test('fromarray many-element', function(t) {
 });
 
 test('fromarray non-array', function(t) {
-  t.equal(fromarray(undefined) === undefined, true);
-  t.equal(fromarray(null) === null, true);
+  t.equal(fromarray(undefined), undefined);
+  t.equal(fromarray(null), null);
 
-  t.equal(fromarray(false) === false, true);
-  t.equal(fromarray(true) === true, true);
+  t.equal(fromarray(false), false);
+  t.equal(fromarray(true), true);
   t.equal(fromarray(0), 0);
   t.equal(fromarray(1), 1);
   t.deepEqual(fromarray({}), {});
   t.deepEqual(fromarray({1:2, 3:4}), {1:2, 3:4});
   t.equal(fromarray(3.14), 3.14);
+
+  t.end();
+});
+
+test('fromarray array-like', function(t) {
+  t.equal(fromarray({length:0}), undefined);
+  t.equal(fromarray({length:1, 0:4}), 4);
 
   t.end();
 });
